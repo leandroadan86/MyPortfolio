b6522c2665884a7428d85bb6bda5e8116eb7b3de
Leandro adan <83481475+leandroadan86@users.noreply.github.com>
2022-01-23 19:53:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-const moon = document.getElementById("moon");
-moon.addEventListener("click", changeImage);
-foto=1
-function changeImage() {
-    if(foto==1){
-        moon.src="./images/bluemoon.png";
-        foto--;
-        }
-    else{
-        moon.src="./images/moon2.png";
-        foto==1;
-        foto++;
-        }}
 
+
+const moon = document.getElementById("moon");
+    moon.addEventListener("click", changeImage);
+    foto=1
+    function changeImage() {
+        if(foto==1){
+            moon.src="./images/bluemoon.png";
+            foto--;
+            }
+        else{
+            moon.src="./images/moon2.png";
+            foto==1;
+            foto++;
+            }}
+
+
+window.addEventListener('scroll', function(){
+   
+    var bloque = document.querySelectorAll('.animated');
+
+    for (var i = 0; i < bloque.length; i++) {   
+        var altura = window.innerHeight/1.9;
+        var distancia = bloque[i].getBoundingClientRect().top;
+
+        if(distancia <= altura){
+            bloque[i].classList.add('aparece'); }
+        else{
+            bloque[i].classList.remove('aparece'); }
+    
+    }   }   )
